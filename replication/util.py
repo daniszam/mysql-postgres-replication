35ba9e787e650c3d178b34eda066f75d30b78962
Danis Zamaliev <daniszamaliev@gmail.com>
2021-04-17 15:17:32 +0300

    add configuration file

--- replication/util.py
+++ replication/util.py
@@ -1,3 +1,10 @@
+import logging
+from pathlib import Path
+
+import yaml
+
+from replication.connection import Connection
+
 type_dictionary = {
     'integer': 'integer',
     'mediumint': 'bigint',
@@ -34,3 +41,37 @@ type_dictionary = {
     'bool': 'boolean',
     'boolean': 'boolean',
 }
+
+
+def get_path(str_path: str):
+    try:
+        return Path(str_path)
+    except NotImplementedError:
+        logging.error("Could not be resolved path=%s", str_path)
+
+
+def get_configuration(path: Path) -> {}:
+    with path.open() as configuration_file:
+        configuration = yaml.load(configuration_file, Loader=yaml.FullLoader)
+        logging.debug("read yaml file=%s", configuration)
+        return configuration
+
+
+def get_connection(configuration: {}) -> Connection:
+    mysql_configuration = configuration['example']['from']['mysql_1']
+    connection = Connection(
+        host=mysql_configuration['host'],
+        user=mysql_configuration['user'],
+        port=mysql_configuration['port'],
+        password=mysql_configuration['password'],
+        timeout=mysql_configuration['timeout'],
+        charset=mysql_configuration['charset']
+    )
+    return connection
+
+
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+path = get_path("example.yaml")
+configuration = get_configuration(path)
+connection = get_connection(configuration)
+print(connection)
